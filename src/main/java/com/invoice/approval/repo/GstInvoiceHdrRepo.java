a58d7f908932e2f6dcea2f224f494570ab7e3e97
Mano <mano@whydigit.in>
2024-12-09 21:49:28 +0530

    invoice sql updated

--- src/main/java/com/invoice/approval/repo/GstInvoiceHdrRepo.java
+++ src/main/java/com/invoice/approval/repo/GstInvoiceHdrRepo.java
@@ -11,18 +11,19 @@ import com.invoice.approval.entity.GstInvoiceHdrVO;
 public interface GstInvoiceHdrRepo extends JpaRepository<GstInvoiceHdrVO, Long> {
 
 	
-	@Query(nativeQuery = true,value = "select gst_invoicehdrid,branchcode,finyr,docid,docdt,Partyname,partycode,outstanding,totinvamtlc,creditdays,creditlimit,approve1,approve2,approve3,approve1name,approve1on from gst_invoicehdr \r\n"
-			+ "where invproceed = 'F' and approve1 = 'F' and approve1name is null"
-			+ " AND branchcode in (select branchcode from vg_userbranch where userName =?1)  order by createdon desc")
+	@Query(nativeQuery = true,value = "select gst_invoicehdrid,a.branchcode,finyr,docid,docdt,Partyname,partycode,outstanding,totinvamtlc,"
+			+ "b.creditdays,b.creditlimit,approve1,approve2,approve3,approve1name,approve1on from gst_invoicehdr a,mg_partyhdr b \r\n"
+			+ "where invproceed = 'F' and approve1 = 'F' and a.partycode = b.party_code and approve1name is null"
+			+ " AND a.branchcode in (select branchcode from vg_userbranch where userName =?1)  order by a.createdon desc")
 	Set<Object[]> getPendingDetailsApprove1(String userName);
 
 	@Query(value = "select a from GstInvoiceHdrVO a where a.gstInvoiceHdrId=?1")
 	GstInvoiceHdrVO findByGstInvoiceHdrId(Long id);
 	
-	@Query(nativeQuery = true,value = "select gst_invoicehdrid,branchcode,finyr,docid,docdt,Partyname,partycode,outstanding,totinvamtlc,creditdays,creditlimit,approve1,approve1name,approve1on,approve2,approve2name,approve2on,approve3,approve3name,approve3on \r\n"
-			+ "from gst_invoicehdr \r\n"
-			+ "where invproceed = 'F' and approve1 = 'T' and approve2='F' and approve2name is null "
-			+ "AND branchcode in (select branchcode from vg_userbranch where userName =?1) order by createdon desc")
+	@Query(nativeQuery = true,value = "select gst_invoicehdrid,a.branchcode,finyr,docid,docdt,Partyname,partycode,outstanding,totinvamtlc,b.creditdays,b.creditlimit,approve1,approve1name,approve1on,approve2,approve2name,approve2on,approve3,approve3name,approve3on \r\n"
+			+ "from gst_invoicehdr a,mg_partyhdr b \r\n"
+			+ "where invproceed = 'F' and approve1 = 'T' and a.partycode = b.party_code and  approve2='F' and approve2name is null "
+			+ "AND a.branchcode in (select branchcode from vg_userbranch where userName =?1) order by a.createdon desc")
 	Set<Object[]> getPendingDetailsApprove2(String userName);
 	
 	
